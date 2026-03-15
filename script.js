2d2cd1d69d84566bf493a25e2880405adea3233b
infddgc <mikerrrr24@gmail.com>
2026-03-16 02:08:32 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+﻿var map = L.map('mapbox').setView([40.4093,49.8671],7);
+
+L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+).addTo(map);
+
+var restaurants=[];
+
+fetch("https://overpass-api.de/api/interpreter?data=[out:json];area[name='Azerbaijan'];node[amenity=restaurant](area);out;")
+.then(res=>res.json())
+.then(data=>{
+
+data.elements.forEach(place=>{
+
+if(place.lat && place.lon){
+
+var name=place.tags?.name || "Restaurant";
+
+restaurants.push({
+name:name,
+lat:place.lat,
+lon:place.lon
+});
+
+L.marker([place.lat,place.lon])
+.addTo(map)
+.bindPopup(name);
+
+}
+
+});
+
+});
+
+var search=document.getElementById("search");
+
+search.addEventListener("input",function(){
+
+var value=search.value.toLowerCase();
+
+restaurants.forEach(r=>{
+
+if(r.name.toLowerCase().includes(value)){
+
+map.setView([r.lat,r.lon],14);
+
+}
+
+});
+
+});
