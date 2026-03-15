d641ca376bc957f74087ad6ffd17ed8eeb9ed3a8
infddgc <mikerrrr24@gmail.com>
2026-03-16 02:06:35 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-﻿ var map = L.map('mapbox').setView([40.4093, 49.8671], 12);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-}).addTo(map);
-
-L.marker([40.4093, 49.8671]).addTo(map)
-.bindPopup("Restaurant example")
-.openPopup();
